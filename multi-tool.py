c5e775e8bb3184e3a5c7564619f1894223764f37
opsecking90 <opsec70@tutamail.com>
2026-05-27 23:24:22 +0200

    Cal v1

---
+++ multi-tool.py
@@ -0,0 +1,458 @@
+
+import time
+import random
+
+
+def cal():
+    first = float(input("first number: "))
+    op = input("+ - / :")
+    first2 = float(input("number 2: "))
+
+    if op == "+":
+        print (first + first2)
+
+    elif op == "-":
+        print(first - first2)
+
+    elif op == "*":
+        print (first * first2)
+
+    elif op == "/":
+        print (first / first2)
+
+
+
+
+def rage():
+    for i in range(7):
+        print ("NO!")
+        time.sleep(0.2)
+
+
+def cal2():
+
+        print("CALCULATOR BY HACKER MAN")
+        time.sleep (0.7)
+        num1 = int(input("number 1:  "))
+        op = (input("ONLY + FOR CAL:  "))
+        num2 = int(input("number 2:  "))
+
+
+
+        b = num1 + num2
+
+        if b == 1:
+            print("it was 1")
+        elif b == 2:
+            print("it was 2")
+        elif b == 3:
+            print("it was 3")
+        elif b == 4:
+            print("it was 4")
+        elif b == 5:
+            print("it was 5")
+        elif b == 6:
+            print("it was 6")
+        elif b == 7:
+            print("it was 7")
+        elif b == 8:
+            print("it was 8")
+        elif b == 9:
+            print("it was 9")
+        elif b == 10:
+            print("it was 10")
+        elif b == 11:
+            print("it was 11")
+        elif b == 12:
+            print("it was 12")
+        elif b == 13:
+            print("it was 13")
+        elif b == 14:
+            print("it was 14")
+        elif b == 15:
+            print("it was 15")
+        elif b == 16:
+            print("it was 16")
+        elif b == 17:
+            print("it was 17")
+        elif b == 18:
+            print("it was 18")
+        elif b == 19:
+            print("it was 19")
+        elif b == 20:
+            print("it was 20")
+        elif b == 21:
+            print("it was 21")
+        elif b == 22:
+            print("it was 22")
+        elif b == 23:
+            print("it was 23")
+        elif b == 24:
+            print("it was 24")
+        elif b == 25:
+            print("it was 25")
+        elif b == 26:
+            print("it was 26")
+        elif b == 27:
+            print("it was 27")
+        elif b == 28:
+            print("it was 28")
+        elif b == 29:
+            print("it was 29")
+        elif b == 30:
+            print("it was 30")
+        elif b == 31:
+            print("it was 31")
+        elif b == 32:
+            print("it was 32")
+        elif b == 33:
+            print("it was 33")
+        elif b == 34:
+            print("it was 34")
+        elif b == 35:
+            print("it was 35")
+        elif b == 36:
+            print("it was 36")
+        elif b == 37:
+            print("it was 37")
+        elif b == 38:
+            print("it was 38")
+        elif b == 39:
+            print("it was 39")
+        elif b == 40:
+            print("it was 40")
+        elif b == 41:
+            print("it was 41")
+        elif b == 42:
+            print("it was 42")
+        elif b == 43:
+            print("it was 43")
+        elif b == 44:
+            print("it was 44")
+        elif b == 45:
+            print("it was 45")
+        elif b == 46:
+            print("it was 46")
+        elif b == 47:
+            print("it was 47")
+        elif b == 48:
+            print("it was 48")
+        elif b == 49:
+            print("it was 49")
+        elif b == 50:
+            print("it was 50")
+        elif b == 51:
+            print("it was 51")
+        elif b == 52:
+            print("it was 52")
+        elif b == 53:
+            print("it was 53")
+        elif b == 54:
+            print("it was 54")
+        elif b == 55:
+            print("it was 55")
+        elif b == 56:
+            print("it was 56")
+        elif b == 57:
+            print("it was 57")
+        elif b == 58:
+            print("it was 58")
+        elif b == 59:
+            print("it was 59")
+        elif b == 60:
+            print("it was 60")
+        elif b == 61:
+            print("it was 61")
+        elif b == 62:
+            print("it was 62")
+        elif b == 63:
+            print("it was 63")
+        elif b == 64:
+            print("it was 64")
+        elif b == 65:
+            print("it was 65")
+        elif b == 66:
+            print("it was 66")
+        elif b == 67:
+            print("it was 67")
+        elif b == 68:
+            print("it was 68")
+        elif b == 69:
+            print("it was 69")
+        elif b == 70:
+            print("it was 70")
+        elif b == 71:
+            print("it was 71")
+        elif b == 72:
+            print("it was 72")
+        elif b == 73:
+            print("it was 73")
+        elif b == 74:
+            print("it was 74")
+        elif b == 75:
+            print("it was 75")
+        elif b == 76:
+            print("it was 76")
+        elif b == 77:
+            print("it was 77")
+        elif b == 78:
+            print("it was 78")
+        elif b == 79:
+            print("it was 79")
+        elif b == 80:
+            print("it was 80")
+        elif b == 81:
+            print("it was 81")
+        elif b == 82:
+            print("it was 82")
+        elif b == 83:
+            print("it was 83")
+        elif b == 84:
+            print("it was 84")
+        elif b == 85:
+            print("it was 85")
+        elif b == 86:
+            print("it was 86")
+        elif b == 87:
+            print("it was 87")
+        elif b == 88:
+            print("it was 88")
+        elif b == 89:
+            print("it was 89")
+        elif b == 90:
+            print("it was 90")
+        elif b == 91:
+            print("it was 91")
+        elif b == 92:
+            print("it was 92")
+        elif b == 93:
+            print("it was 93")
+        elif b == 94:
+            print("it was 94")
+        elif b == 95:
+            print("it was 95")
+        elif b == 96:
+            print("it was 96")
+        elif b == 97:
+            print("it was 97")
+        elif b == 98:
+            print("it was 98")
+        elif b == 99:
+            print("it was 99")
+        elif b == 100:
+            print("it was 100")
+        elif b == 101:
+            print("it was 101")
+        elif b == 102:
+            print("it was 102")
+        elif b == 103:
+            print("it was 103")
+        elif b == 104:
+            print("it was 104")
+        elif b == 105:
+            print("it was 105")
+        elif b == 106:
+            print("it was 106")
+        elif b == 107:
+            print("it was 107")
+        elif b == 108:
+            print("it was 108")
+        elif b == 109:
+            print("it was 109")
+        elif b == 110:
+            print("it was 110")
+        elif b == 111:
+            print("it was 111")
+        elif b == 112:
+            print("it was 112")
+        elif b == 113:
+            print("it was 113")
+        elif b == 114:
+            print("it was 114")
+        elif b == 115:
+            print("it was 115")
+        elif b == 116:
+            print("it was 116")
+        elif b == 117:
+            print("it was 117")
+        elif b == 118:
+            print("it was 118")
+        elif b == 119:
+            print("it was 119")
+        elif b == 120:
+            print("it was 120")
+        elif b == 121:
+            print("it was 121")
+        elif b == 122:
+            print("it was 122")
+        elif b == 123:
+            print("it was 123")
+        elif b == 124:
+            print("it was 124")
+        elif b == 125:
+            print("it was 125")
+        elif b == 126:
+            print("it was 126")
+        elif b == 127:
+            print("it was 127")
+        elif b == 128:
+            print("it was 128")
+        elif b == 129:
+            print("it was 129")
+        elif b == 130:
+            print("it was 130")
+        elif b == 131:
+            print("it was 131")
+        elif b == 132:
+            print("it was 132")
+        elif b == 133:
+            print("it was 133")
+        elif b == 134:
+            print("it was 134")
+        elif b == 135:
+            print("it was 135")
+        elif b == 136:
+            print("it was 136")
+        elif b == 137:
+            print("it was 137")
+        elif b == 138:
+            print("it was 138")
+        elif b == 139:
+            print("it was 139")
+        elif b == 140:
+            print("it was 140")
+        elif b == 141:
+            print("it was 141")
+        elif b == 142:
+            print("it was 142")
+        elif b == 143:
+            print("it was 143")
+        elif b == 144:
+            print("it was 144")
+        elif b == 145:
+            print("it was 145")
+        elif b == 146:
+            print("it was 146")
+        elif b == 147:
+            print("it was 147")
+        elif b == 148:
+            print("it was 148")
+        elif b == 149:
+            print("it was 149")
+        elif b == 150:
+            print("it was 150")
+        elif b == 151:
+            print("it was 151")
+        elif b == 152:
+            print("it was 152")
+        elif b == 153:
+            print("it was 153")
+        elif b == 154:
+            print("it was 154")
+        elif b == 155:
+            print("it was 155")
+        elif b == 156:
+            print("it was 156")
+        elif b == 157:
+            print("it was 157")
+        elif b == 158:
+            print("it was 158")
+        elif b == 159:
+            print("it was 159")
+        elif b == 160:
+            print("it was 160")
+        elif b == 161:
+            print("it was 161")
+        elif b == 162:
+            print("it was 162")
+        elif b == 163:
+            print("it was 163")
+        elif b == 164:
+            print("it was 164")
+        elif b == 165:
+            print("it was 165")
+        elif b == 166:
+            print("it was 166")
+        elif b == 167:
+            print("it was 167")
+        elif b == 168:
+            print("it was 168")
+        elif b == 169:
+            print("it was 169")
+        elif b == 170:
+            print("it was 170")
+        elif b == 171:
+            print("it was 171")
+        elif b == 172:
+            print("it was 172")
+        elif b == 173:
+            print("it was 173")
+        elif b == 174:
+            print("it was 174")
+        elif b == 175:
+            print("it was 175")
+        elif b == 176:
+            print("it was 176")
+        elif b == 177:
+            print("it was 177")
+        elif b == 178:
+            print("it was 178")
+        elif b == 179:
+            print("it was 179")
+        elif b == 180:
+            print("it was 180")
+        elif b == 181:
+            print("it was 181")
+        elif b == 182:
+            print("it was 182")
+        elif b == 183:
+            print("it was 183")
+        elif b == 184:
+            print("it was 184")
+        elif b == 185:
+            print("it was 185")
+        elif b == 186:
+            print("it was 186")
+        elif b == 187:
+            print("it was 187")
+        elif b == 188:
+            print("it was 188")
+        elif b == 189:
+            print("it was 189")
+        elif b == 190:
+            print("it was 190")
+        elif b == 191:
+            print("it was 191")
+        elif b == 192:
+            print("it was 192")
+        elif b == 193:
+            print("it was 193")
+        elif b == 194:
+            print("it was 194")
+        elif b == 195:
+            print("it was 195")
+        elif b == 196:
+            print("it was 196")
+        elif b == 197:
+            print("it was 197")
+        elif b == 198:
+            print("it was 198")
+        elif b == 199:
+            print("it was 199")
+        elif b == 200:
+            print("it was 200")
+        else:
+            print("NOOOOOO PC IS EXPLODING TO BIG")
+            rage()
+
+
+
+time.sleep(0.7)
+print ("Multi - tool")
+
+sel = input("cal:")
+
+if sel == "cal":
+    cal()
+else:
+    cal2()
